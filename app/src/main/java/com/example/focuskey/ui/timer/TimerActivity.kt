9f6fb59d33c8bad73007b7967a3a595c6500f83b
Alexvan1 <quatercercle03@gmail.com>
2025-12-05 21:21:40 +0300

    Initial commit

---
+++ app/src/main/java/com/example/focuskey/ui/timer/TimerActivity.kt
@@ -0,0 +1,42 @@
+package com.example.focuskey.ui.timer
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.focuskey.databinding.FragmentTimerBinding
+
+class TimerActivity : Fragment() {
+
+    private var _binding: FragmentTimerBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    @SuppressLint("SetTextI18n")
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val TimerLayout =
+            ViewModelProvider(this).get(TimerLayout::class.java)
+
+        _binding = FragmentTimerBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
